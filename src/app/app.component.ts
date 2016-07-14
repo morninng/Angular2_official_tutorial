4bd7c0af03b526d1ebd63d62acec7ffb31704ad8
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-07-14 17:56:15 +0900

    tutorial 4 multiple components has been added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeroDetailComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
@@ -20,10 +23,6 @@ export class AppComponent {
 
 }
 
-export class Hero {
-  id: number;
-  name: string;
-}
 
 
 const HEROES: Hero[] = [
